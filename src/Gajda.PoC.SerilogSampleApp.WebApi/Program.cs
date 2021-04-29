7726e841525b2c4893b74219a4703f5fd42abff0
Michal Gajda <michal@gajda.co.uk>
2021-04-29 12:08:54 +0100

    Init

---
+++ src/Gajda.PoC.SerilogSampleApp.WebApi/Program.cs
@@ -0,0 +1,51 @@
+namespace Gajda.PoC.SerilogSampleApp.WebApi
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+    using Serilog;
+    using Serilog.Sinks.Graylog;
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .WriteTo.Graylog(new GraylogSinkOptions
+                {
+                    HostnameOrAddress = Environment.GetEnvironmentVariable("GRAYLOG_ADDRESS") ?? "localhost",
+                    Port = 12201,
+                })
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Application is starting up...");
+                await CreateHostBuilder(args).Build().RunAsync();
+            }
+            catch (Exception exception)
+            {
+                Log.Fatal(exception, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ src/Gajda.PoC.SerilogSampleApp.WebApi/Startup.cs
@@ -0,0 +1,55 @@
+namespace Gajda.PoC.SerilogSampleApp.WebApi
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.OpenApi.Models;
+    using Serilog;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            this.Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddSerilog());
+            services.AddHealthChecks();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Serilog Sample App",
+                    Version = "v1",
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("v1/swagger.json", "Serilog Sample App v1"));
+            }
+
+            app.UseHealthChecks("/health");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
